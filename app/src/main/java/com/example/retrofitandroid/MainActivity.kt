b4fa4b61faf59c55b713798a3944dcf4cde27fe1
sanskar-pawar <sanskar.pawar@coditas.com>
2022-02-24 13:08:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrofitandroid/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.retrofitandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+            getMyData()
+
+
+    }
+
+    private fun getMyData() {
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+
+
+        val retrofitData = retrofitBuilder.getData()
+
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+            override fun onResponse(
+                call: Call<List<MyDataItem>?>,
+                response: Response<List<MyDataItem>?>
+            ) {
+
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/retrofitandroid/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.retrofitandroid
+
+class MyData : ArrayList<MyDataItem>()
